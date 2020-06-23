6634602f6616bc5cdf8486705fab8f303439cb08
Joe Sauve <joe@joesauve.com>
2020-06-23 10:25:05 -0500

    Removed Android project. Removed some cruft from iOS project.

--- com.spectrum.UserLog/com.spectrum.UserLog.iOS/Setup.cs
+++ com.spectrum.UserLog/com.spectrum.UserLog.iOS/Setup.cs
@@ -1,25 +1,12 @@
 using com.spectrum.UserLog.Core;
-using MvvmCross;
-using MvvmCross.Base;
-using MvvmCross.Binding.Bindings.Target.Construction;
 using MvvmCross.IoC;
 using MvvmCross.Platforms.Ios.Core;
-using MvvmCross.Platforms.Ios.Presenters;
 using MvvmCross.ViewModels;
-using UIKit;
 
 namespace com.spectrum.UserLog.iOS
 {
     public class Setup : MvxIosSetup<App>
     {
-        protected override void InitializeLastChance()
-        {
-            base.InitializeLastChance();
-
-            var registry = Mvx.IoCProvider.Resolve<IMvxTargetBindingFactoryRegistry>();
-            registry.RegisterFactory(new MvxCustomBindingFactory<UIViewController>("NetworkIndicator", (viewController) => new NetworkIndicatorTargetBinding(viewController)));
-        }
-
         protected override IMvxApplication CreateApp()
         {
             return new App();
