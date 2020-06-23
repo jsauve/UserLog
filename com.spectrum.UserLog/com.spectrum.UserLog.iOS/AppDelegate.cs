239fd0d8b73aa918f076d37cd035711e59fc4f1b
Joe Sauve <joe@joesauve.com>
2020-06-23 01:08:48 -0500

    Removed commented code in AppDelegate

--- com.spectrum.UserLog/com.spectrum.UserLog.iOS/AppDelegate.cs
+++ com.spectrum.UserLog/com.spectrum.UserLog.iOS/AppDelegate.cs
@@ -8,26 +8,5 @@ namespace com.spectrum.UserLog.iOS
     [Register("AppDelegate")]
     public partial class AppDelegate : MvxApplicationDelegate<Setup, App>
     {
-    
-    //public override UIWindow Window
-    //{
-    //    get;
-    //    set;
-    //}
-
-    //public override bool FinishedLaunching(UIApplication app, NSDictionary options)
-    //{
-    //    Window = new UIWindow(UIScreen.MainScreen.Bounds);
-
-    //    var setup = new Setup(this, Window);
-    //    setup.Initialize();
-
-    //    var startup = Mvx.Resolve<IMvxAppStart>();
-    //    startup.Start();
-
-    //    Window.MakeKeyAndVisible();
-
-    //    return true;
-    //}
-}
+    }
 }
